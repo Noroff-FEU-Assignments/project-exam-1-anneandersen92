34785b6182e913f667ce36ed02bd90a6e3a70054
anneandersen92 <anne_92@live.no>
2023-11-03 14:30:38 +0100

    working blog api call

---
+++ js/fetch/blog.js
@@ -0,0 +1,49 @@
+// import { displayError } from "./components/errorMessage.js";
+
+const url = "https://annesflower.no/ohsheet/wp-json/wp/v2/posts/";
+const completeUrl = url + "?_embed&per_page=100";
+
+const largePosts = document.querySelector(".large-posts");
+const smallPosts = document.querySelector(".small-posts");
+
+async function fetchBlog() {
+    try {
+        const response = await fetch(completeUrl);
+        const blog = await response.json();
+
+        largePosts.innerHTML = "";
+
+        for (let i = 0; i < blog.length; i++) {
+            if (i === 3) { break; }
+            const post = blog[i];
+            createLargePosts(post);
+        };
+
+        function createLargePosts(post) {
+            largePosts.innerHTML += `<a href="blogpost.html?id=${post.id}" style="background-image: url(${post._embedded["wp:featuredmedia"][0].source_url});" alt="${post._embedded["wp:featuredmedia"][0].alt_text}" class="small-post pad-all rounded">
+                                            <h2 class="text-white-80 justify-r">Post</h2>
+                                            <h3 class="bg-light-beige rounded">${post.title.rendered}</h3>
+                                    </a>`;
+        };
+
+        smallPosts.innerHTML = "";
+
+        for (let i = 3; i < blog.length; i++) {
+            const post = blog[i];
+            createSmallPosts(post);
+        };
+
+        function createSmallPosts(post) {
+            smallPosts.innerHTML += `<a href="blogpost.html?id=${post.id}" style="background-image: url(${post._embedded["wp:featuredmedia"][0].source_url});" alt="${post._embedded["wp:featuredmedia"][0].alt_text}" class="small-post pad-all rounded">
+                                            <h2 class="text-white-80 justify-r">Post</h2>
+                                            <h3 class="bg-light-beige rounded">${post.title.rendered}</h3>
+                                    </a>`;
+        };
+
+    } catch (error) {
+        largePosts.innerHTML = `<p>An error occured when calling the API</div`;
+        smallPosts.innerHTML = `<p>An error occured when calling the API</div`;
+    }
+}
+
+fetchBlog();
--- js/script.js
+++ js/script.js
@@ -1,38 +1,3 @@
 import { createHeader } from "./components/header.js";
 import { createFooter } from "./components/footer.js";
 import { imgModal } from "./components/imgModal.js";
-import { checkButton, submitForm } from "./components/formValidation.js";
-
-// import { displayError } from "./components/errorMessage.js";
-
-const url = "https://annesflower.no/ohsheet/wp-json/wp/v2/posts/";
-const completeUrl = url + "?per_page=100";
-
-const largePosts = document.querySelector(".large-posts");
-const smallPosts = document.querySelector(".small-posts");
-
-async function fetchProducts() {
-    try {
-        const response = await fetch(completeUrl);
-        const products = await response.json();
-
-        smallPosts.innerHTML = "";
-
-        for (let i = 0; i < products.length; i++) {
-
-            const product = products[i];
-        };
-
-        function createHtml(product) {
-            smallPosts.innerHTML += `<a href="product.html?id=${product.id}" style="background-image: url(${product._embedded["wp:featuredmedia"][0].source_url});" alt="${product._embedded["wp:featuredmedia"][0].alt_text}" class="small-post pad-all rounded">
-                                            <h2 class="text-white-80 justify-r">Post</h2>
-                                            <h3 class="bg-light-beige rounded">${product.title.rendered}</h3>
-                                    </a>`;
-        };
-
-    } catch (error) {
-        smallPosts.innerHTML = `<p>An error occured when calling the API</div`;
-    }
-}
-
-fetchProducts();
